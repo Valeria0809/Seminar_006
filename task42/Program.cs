da1355de246ca34cd933373e4d1dcc2141c729be
VALERIA <valeri.tereshenkova@icloud.com>
2023-05-28 12:15:58 +0300

    Task 42

--- task42/Program.cs
+++ task42/Program.cs
@@ -4,3 +4,23 @@
 //3 -> 11
 //2 -> 10
 
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+
+}
+
+void ConvertToBinary(int number)
+{
+if(number <= 0)
+{
+    return;
+}
+    ConvertToBinary(number/2);
+    System.Console.Write(number % 2);
+}
+
+
+int number = ReadInt("Введите число: ");
+ConvertToBinary(number);
